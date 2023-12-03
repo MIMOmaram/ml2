0ba4d6d9ebf0bbe2914d8286a83dce866abbbc74
MIMOmaram <marayari123@gmail.com>
2023-12-03 16:01:17 +0100

    changer import

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
 st.write('''
